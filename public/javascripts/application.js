6eef5486414c2406bf6b3b67d1982e9e2cf99e65
Adam McCrea <adam@adamlogic.com>
2010-01-07 13:42:06 -0500

    allow user to specify range

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,22 @@
 $(function() {
 
-  $('.result').solari('09876');
+  $('form#range').submit(function() {
+    var begin = parseInt($('#rangebegin').val()),
+        end = parseInt($('#rangeend').val()),
+        result = $.rand(begin, end);
+
+    $('.result').solari(result);
+    return false
+  });
 
 });
+
+$.rand = function() {
+  switch(arguments.length) {
+    case 1:
+      return Math.floor(Math.random() * arguments[0]);
+    case 2:
+      var begin = arguments[0], end = arguments[1];
+      return $.rand(end - begin + 1) + begin;
+  }
+}
