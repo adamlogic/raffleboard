0bae254e4d4066f9b8710106b7e8fb38867587b5
Adam McCrea <adam@adamlogic.com>
2010-01-07 16:35:58 -0500

    use multiple ranges

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,17 @@
 $(function() {
 
   $('form#range').submit(function() {
-    var begin = parseInt($('#rangebegin').val()),
-        end = parseInt($('#rangeend').val()),
-        result = $.rand(begin, end);
+    var begin1 = parseInt($('#range1begin').val()),
+        end1 = parseInt($('#range1end').val()),
+        begin2 = parseInt($('#range2begin').val()),
+        end2 = parseInt($('#range2end').val()),
+        possibilities = [];
+        
 
-    $('.result').solari(result);
+    for (i=begin1; i<=end1; i++) { possibilities.push(i) };
+    for (i=begin2; i<=end2; i++) { possibilities.push(i) };
+
+    $('.result').solari($.rand(possibilities));
     return false
   });
 
@@ -14,9 +20,15 @@ $(function() {
 $.rand = function() {
   switch(arguments.length) {
     case 1:
-      return Math.floor(Math.random() * arguments[0]);
+      if ($.isArray(arguments[0])) {
+        return $.rand.apply(this, arguments[0]);
+      } else {
+        return Math.floor(Math.random() * arguments[0]);
+      }
     case 2:
       var begin = arguments[0], end = arguments[1];
       return $.rand(end - begin + 1) + begin;
+    default:
+      return arguments[$.rand(arguments.length)];
   }
 }
